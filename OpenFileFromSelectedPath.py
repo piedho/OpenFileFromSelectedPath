11fe458535e7ba704473f4ef41e2caf87faba501
piedho <40827143+piedho@users.noreply.github.com>
2021-04-23 23:15:11 +0200

    add is_visible method + multiple selection support
    Add the is_visible(self, *args) method to the plugin : Allows to hide the menu entry when no text is selected.
    Update run(self, ) method to handle multiple selections.

--- OpenFileFromSelectedPath.py
+++ OpenFileFromSelectedPath.py
@@ -4,20 +4,25 @@ import sublime_plugin
 
 
 class OpenFileFromSelectedPathCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		sels = self.view.sel()
+    def run(self, edit):
+        sels = self.view.sel()  # get the first selection
 
-		for sel in sels:
-			s = self.view.substr(sel) # get the selected text as str
-			
-			# Get current directory, to check if file exists locally 
-			current_dir = self.view.window().extract_variables()['file_path']
-			local_path = current_dir + os.path.sep + s 
-			
-			# If file exists, open it
-			if os.path.exists(local_path):
-				self.view.window().open_file(local_path)
-			elif os.path.exists(s):
-				self.view.window().open_file(s)
+        # Get current directory
+        current_dir = self.view.window().extract_variables()['file_path']
+        
+        for sel in sels:
+            # Get the selected text as str
+            s = self.view.substr(sel) 
 
-			break
+            if s:  # if str not empty
+                local_path = current_dir + os.path.sep + s 
+                
+                # If file exists, open it
+                if os.path.isfile(local_path):
+                    self.view.window().open_file(local_path)
+                elif os.path.isfile(s):
+                    self.view.window().open_file(s)
+
+    def is_visible(self, *args):
+        sel = self.view.sel()[0]
+        return not sel.empty()

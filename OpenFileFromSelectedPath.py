4797711c17810a08016ebb5a2840014e1189a997
piedho <40827143+piedho@users.noreply.github.com>
2021-04-23 23:38:36 +0200

    Add support for buffers/not-saved files

--- OpenFileFromSelectedPath.py
+++ OpenFileFromSelectedPath.py
@@ -5,24 +5,33 @@ import sublime_plugin
 
 class OpenFileFromSelectedPathCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        sels = self.view.sel()  # get the first selection
+        sels = self.view.sel()
 
         # Get current directory
-        current_dir = self.view.window().extract_variables()['file_path']
+        current_dir = self.view.window().extract_variables().get('file_path')
         
         for sel in sels:
             # Get the selected text as str
-            s = self.view.substr(sel) 
+            s = self.view.substr(sel)
 
             if s:  # if str not empty
-                local_path = current_dir + os.path.sep + s 
+                # If we have a current dir (=the calling file is saved), try to open the path locally
+                if current_dir is not None:
+                    local_path = current_dir + os.path.sep + s 
                 
-                # If file exists, open it
-                if os.path.isfile(local_path):
-                    self.view.window().open_file(local_path)
-                elif os.path.isfile(s):
+                    # If file exists, open it
+                    if os.path.isfile(local_path):
+                        self.view.window().open_file(local_path)
+                        continue
+
+                # If str contains an absolute path, open the file
+                if os.path.isfile(s):
                     self.view.window().open_file(s)
 
     def is_visible(self, *args):
         sel = self.view.sel()[0]
         return not sel.empty()
+
+
+   
+
